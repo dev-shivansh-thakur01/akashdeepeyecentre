81f4e0abee85d1f9cab5733b561f11a955a3b490
dev-shivansh-thakur01 <dev.shivanshthakur@gmail.com>
2026-02-21 13:01:40 +0530

    Migration Updates

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: '.next_new',
   typescript: {
     // ignoreBuildErrors: true, // Removed for production readiness
   },
